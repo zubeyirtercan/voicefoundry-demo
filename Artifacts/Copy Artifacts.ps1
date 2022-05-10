9d65b3d3e5896413dc59014ccfdbfc41b1dff352
Zubeyir Tercan <zubeyir.tercan@scalefocus.com>
2022-05-10 13:49:48 +0300

    Note added to the copy batch

--- Artifacts/Copy Artifacts.ps1
+++ Artifacts/Copy Artifacts.ps1
@@ -1,6 +1,7 @@
 # This batch copies all the artifacts from voicefoundry-artifacts bucket to artifact buckets in every region
 # so that the Lambda creation block in CloudFormation template can work in any region that supports Amazon Connect
-# voicefoundry-artifacts bucket is a bucket that has been created in the author's AWS account
+# voicefoundry-artifacts bucket is a bucket that has been created in the author's AWS account.
+# This script is intended to be run by the author only.
 
 aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-us-east-1
 aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-us-west-2
