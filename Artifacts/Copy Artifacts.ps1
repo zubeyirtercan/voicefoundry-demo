9e8dce38d438cf2452460591837ce54be425fed5
Zubeyir Tercan <zubeyir.tercan@scalefocus.com>
2022-05-30 15:09:10 +0300

    Copy artifacts batch updated

--- Artifacts/Copy Artifacts.ps1
+++ Artifacts/Copy Artifacts.ps1
@@ -3,13 +3,13 @@
 # voicefoundry-artifacts bucket is a bucket that has been created in the author's AWS account.
 # This script is intended to be run by the author only.
 
-aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-us-east-1
-aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-us-west-2
-aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-af-south-1
-aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-ap-northeast-2
-aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-ap-southeast-1
-aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-ap-southeast-2
-aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-ap-northeast-1
-aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-ca-central-1
-aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-eu-central-1
-aws s3 sync s3://voicefoundry-artifacts s3://voicefoundry-artifacts-eu-west-2
+aws s3 sync s3://voicefoundry-artifacts/LambdaFunctions s3://voicefoundry-artifacts-af-south-1/LambdaFunctions
+aws s3 sync s3://voicefoundry-artifacts/LambdaFunctions s3://voicefoundry-artifacts-ap-northeast-1/LambdaFunctions
+aws s3 sync s3://voicefoundry-artifacts/LambdaFunctions s3://voicefoundry-artifacts-ap-northeast-2/LambdaFunctions
+aws s3 sync s3://voicefoundry-artifacts/LambdaFunctions s3://voicefoundry-artifacts-ap-southeast-1/LambdaFunctions
+aws s3 sync s3://voicefoundry-artifacts/LambdaFunctions s3://voicefoundry-artifacts-ap-southeast-2/LambdaFunctions
+aws s3 sync s3://voicefoundry-artifacts/LambdaFunctions s3://voicefoundry-artifacts-ca-central-1/LambdaFunctions
+aws s3 sync s3://voicefoundry-artifacts/LambdaFunctions s3://voicefoundry-artifacts-eu-central-1/LambdaFunctions
+aws s3 sync s3://voicefoundry-artifacts/LambdaFunctions s3://voicefoundry-artifacts-eu-west-2/LambdaFunctions
+aws s3 sync s3://voicefoundry-artifacts/LambdaFunctions s3://voicefoundry-artifacts-us-east-1/LambdaFunctions
+aws s3 sync s3://voicefoundry-artifacts/LambdaFunctions s3://voicefoundry-artifacts-us-west-2/LambdaFunctions
