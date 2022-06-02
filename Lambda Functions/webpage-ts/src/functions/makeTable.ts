e245a0608518dbae64f80027a50af4159affb6a8
Zubeyir Tercan <zubeyir.tercan@scalefocus.com>
2022-06-02 13:38:19 +0300

    Phone number is being partially hidden for privacy purpuses

--- Lambda Functions/webpage-ts/src/functions/makeTable.ts
+++ Lambda Functions/webpage-ts/src/functions/makeTable.ts
@@ -5,9 +5,12 @@ export function makeTable(myArray: ItemList) {
     let s: string = "";
 
     myArray.forEach(item => {
+
+        let phoneNumber: String = item["phoneNumber"] as String ?? "";
+        phoneNumber = phoneNumber.substring(0, phoneNumber.length-5) + "XXXXX";
         
         s += "  <tr>"
-        s += "    <th>" + item["phoneNumber"] + "</th>"
+        s += "    <th>" + phoneNumber + "</th>"
         s += "    <th>" + item["recordDate"] + "</th>"
         s += "    <th>" + item["vanityNumbers"] + "</th>"
         s += "  </tr>"
