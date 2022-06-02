5e4e33b084db335afcdbda4ad6bea9e49f1139fe
Zubeyir Tercan <zubeyir.tercan@scalefocus.com>
2022-06-02 13:39:19 +0300

    RecordDate format changed

--- Lambda Functions/webpage-ts/src/functions/makeTable.ts
+++ Lambda Functions/webpage-ts/src/functions/makeTable.ts
@@ -8,10 +8,12 @@ export function makeTable(myArray: ItemList) {
 
         let phoneNumber: String = item["phoneNumber"] as String ?? "";
         phoneNumber = phoneNumber.substring(0, phoneNumber.length-5) + "XXXXX";
+
+        let recordDate: Date = new Date(item["recordDate"] as number ?? 0);
         
         s += "  <tr>"
         s += "    <th>" + phoneNumber + "</th>"
-        s += "    <th>" + item["recordDate"] + "</th>"
+        s += "    <th>" + recordDate.toISOString() + "</th>"
         s += "    <th>" + item["vanityNumbers"] + "</th>"
         s += "  </tr>"
         
